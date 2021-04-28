f53f8722f1f53e174ecce2e555a016df79b2092f
Daniel Philips <daniel_philips@asl.org>
2021-04-28 16:55:52 +0100

    Update

--- Installer.py
+++ Installer.py
@@ -1,10 +1,18 @@
 import os
 import sys
-import PySimpleGUI as sg
-import binascii as bnsci
 from threading import Thread
 
 filesToMove = ['Installer.py','ReGUI.py']
+remouse3__Installer = Thread(target = lambda: os.system("pip3 install remarkable-mouse"))
+remouse__Installer = Thread(target = lambda: os.system("pip install remarkable-mouse"))
+pysimplegui3__Installer = Thread(target = lambda: os.system("pip3 install PySimpleGUI"))
+pysimplegui__Installer = Thread(target = lambda: os.system("pip install PySimpleGUI"))
+
+remouse3__Installer.start()
+remouse__Installer.start()
+pysimplegui3__Installer.start()
+pysimplegui__Installer.start()
+
 def linux():
     withoutHome = currentPos[currentPos.find('home')+5:len(currentPos)]
     path = '/home/' + withoutHome[0:withoutHome.find('/')+1]
@@ -20,7 +28,7 @@ def linux():
         reply = os.system(command)
 
 
-def darwin():
+def darwin(): # MacOS
     toMoveTo = '/Applications/ReGUI/'
     InstallerPos = currentPos + "/" + filesToMove[0]
     ReGUIPos = currentPos + "/" + filesToMove[1]
@@ -31,15 +39,25 @@ def darwin():
     for command in cmds:
         print(command)
         reply = os.system(command)
-        if(reply == 'mkdir: cannot create directory ‘ReGUI’: File exists'):
-            print("Error, file already exists")
+
+def win32():
+    toMoveTo = '/documents/ReGUI/'
+    InstallerPos = currentPos + "/" + filesToMove[0]
+    ReGUIPos = currentPos + "/" + filesToMove[1]
+    moveInstaller = 'cp ' + InstallerPos + ' ' + toMoveTo
+    moveReGUI = 'cp ' + ReGUIPos + ' ' + toMoveTo
+    cmds = ['cd','cd /documents','mkdir ReGUI','cd ReGUI',]
+    cmds = ['cd','cd Documents','mkdir ' + toMoveTo,'cd ReGUI',moveInstaller,moveReGUI]
+    for command in cmds:
+        print(command)
+        reply = os.system(command)
 
 OS = sys.platform   
 currentPos = str(os.path.abspath(os.getcwd()))
 
 if OS == 'linux':
     linux()
-#elif OS == 'win32':
-    #win32()
+elif OS == 'win32':
+    win32()
 elif OS == 'darwin':
     darwin()
