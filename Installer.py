aa59528b0b27a6c9bc3e47662e7d4bde5ee44a87
Daniel Philips <daniel_philips@asl.org>
2021-04-28 20:36:59 +0100

    Update
    Forced scripts to be executable

--- Installer.py
+++ Installer.py
@@ -3,7 +3,7 @@ import time
 import sys
 from threading import Thread
 
-filesToMove = ['Installer.py','Installer.bat','Installer.command','ReGUI.py','ReGUI.bat','ReGUI.command']
+filesToMove = ['Installer.py','Install.bat','Install.command','ReGUI.py','ReGUI.bat','ReGUI.command']
 
 commands = "pip3 install remarkable-mouse; pip install remarkable-mouse; pip3 install PySimpleGUI; pip install PySimpleGUI"
 commander = Thread(target = lambda: os.system(commands))
@@ -13,8 +13,8 @@ commander.start()
 time.sleep(5)
 
 def linux():
-    fileToMove.remove(1)
-    fileToMove.remove(3)
+    filesToMove.remove('Install.bat')
+    filesToMove.remove('ReGUI.bat')
     withoutHome = currentPos[currentPos.find('home')+5:len(currentPos)]
     path = '/home/' + withoutHome[0:withoutHome.find('/')+1]
     toMoveTo = path + 'Documents/ReGUI/'
@@ -22,32 +22,32 @@ def linux():
     for File in filesToMove:
         prevPos = currentPos + "/" + File
         moveCommands = moveCommands + ('cp ' + prevPos + ' ' + toMoveTo) + ';'
-    cmds = ['cd','cd Documents','mkdir ' + toMoveTo,'cd ReGUI',moveCommands]
+    cmds = ['cd','cd Documents','mkdir ' + toMoveTo,'cd ReGUI',moveCommands,'chmod +x ReGUI.command','chmod +x Install.command']
     for command in cmds:
         reply = os.system(command)
 
 
 def darwin(): # MacOS
-    fileToMove.remove(1)
-    fileToMove.remove(3)
+    filesToMove.remove('Install.bat')
+    filesToMove.remove('ReGUI.bat')
     toMoveTo = '/Applications/ReGUI/'
     moveCommands = ''
     for File in filesToMove:
         prevPos = currentPos + "/" + File
         moveCommands = moveCommands + ('cp ' + prevPos + ' ' + toMoveTo) + ';'
-    cmds = ['cd','cd Applications','mkdir ' + toMoveTo,'cd ReGUI',moveCommands]
+    cmds = ['cd','cd Applications','mkdir ' + toMoveTo,'cd ReGUI',moveCommands,'chmod +x ReGUI.command','chmod +x Install.command']
     for command in cmds:
         reply = os.system(command)
 
 def win32():
-    fileToMove.remove(2)
-    fileToMove.remove(4)
+    filesToMove.remove('Install.command')
+    filesToMove.remove('ReGUI.command')
     toMoveTo = '/Documents/ReGUI/'
     moveCommands = ''
     for File in filesToMove:
         prevPos = currentPos + "/" + File
         moveCommands = moveCommands + ('cp ' + prevPos + ' ' + toMoveTo) + ';'
-    cmds = ['cd','cd Documents','mkdir ' + toMoveTo,'cd ReGUI',moveCommands]
+    cmds = ['cd','cd Documents','mkdir ' + toMoveTo,'cd ReGUI',moveCommands,'chmod +x ReGUI.bat','chmod +x Install.bat']
     for command in cmds:
         reply = os.system(command)
 
